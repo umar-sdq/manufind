8517aeb728c21ec9d77059d245b8c20b6ff7f36f
Umar Siddiqui <2377328@cmontmorency.qc.ca>
2025-09-07 15:37:55 -0400

    Pages de base s'affichent (FRONT)

--- manufind/frontend/src/App.jsx
+++ manufind/frontend/src/App.jsx
@@ -2,7 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import RootLayout from "./Components/RootLayout/RootLayout.jsx";
+import MainPage from './Components/MainPage/MainPage.jsx';
+import LoginForm from './Components/LoginForm/LoginForm.jsx';
+import SignUpForm from './Components/SignupForm/SignUpForm.jsx';
 function App() {
 const router = createBrowserRouter([{
     path: "/",
