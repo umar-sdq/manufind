115b2108f03c55fc0a822a3670a214b6cca89881
Muhammad Rahman Riaz <Rahman@serveur.ca>
2025-09-25 13:41:59 -0500

    [TEST 3] Deploy

--- manufind/frontend/src/Components/MainPage/MainPage.jsx
+++ manufind/frontend/src/Components/MainPage/MainPage.jsx
@@ -1,15 +1,15 @@
-import usineImg from '../../assets/images/usine.jpg';
-import SearchCard from '../searchCard/searchCard';
-import './MainPage.css';
+import usineImg from "../../assets/images/usine.jpg";
+import SearchCard from "../SearchCard/SearchCard";
+import "./MainPage.css";
 
 const MainPage = () => {
   return (
     <div className="main-container">
-      <div className='search-card'>
-        <SearchCard/>
+      <div className="search-card">
+        <SearchCard />
       </div>
     </div>
   );
-}
+};
 
 export default MainPage;
