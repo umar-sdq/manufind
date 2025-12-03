567dd68c99a23dab7e0f4c9d9b58a57af5bef6db
Umar Siddiqui <2377328@cmontmorency.qc.ca>
2025-12-03 16:52:24 -0500

    fix env

--- manufind/backend/supabase.js
+++ manufind/backend/supabase.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
+dotenv.config({ path: path.resolve(__dirname, "./.env") });
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
